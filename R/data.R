80d9a5106e2e5c8788b659cd671ed231e539bd56
nrkoehler <norbert.r.koehler@gmail.com>
2018-01-30 21:34:37 +0100

    update documentatiom

--- R/data.R
+++ R/data.R
@@ -1,18 +1,3 @@
-#' DFG--Kandidierendenliste 2015
-#' @format A data frame with 9695 rows and 9 variables:
-#' \describe{
-#'      \item{Fach}{Nummer des Fachkollegiums}
-#'      \item{Person}{Name und Vorname der kandidierenden Person}
-#'      \item{Sex}{Geschlecht der kandidierenden Person}
-#'      \item{Nr}{Fachnummer}
-#'      \item{Institution}{Institution der kandidierenden Person}
-#'      \item{Von}{vorschlagende Institution}
-#'      \item{AK}{Anzahl kandidierender Personen}
-#'      \item{AP}{Anzahl zu wählender Personen}
-#'      \item{Kategorie}{Kategorisierung der vorschlagenden Institutionen in: Hochschule, Fachgesellschaft, Sonstiges}
-#'      }
-#' @source \url{http://www.dfg.de/download/pdf/dfg_im_profil/gremien/fachkollegien/fk-wahl2015/fkwahl_2015_kandidierendenliste.pdf}
-"df.dfg"
 #' Fachkollegien -- Hauptkategorien
 #' @format A data frame with 48 rows and 2 variables:
 #' \describe{
@@ -29,4 +14,19 @@
 #'      }
 #' @source \url{http://www.dfg.de/download/pdf/dfg_im_profil/gremien/fachkollegien/fk-wahl2015/2015_systematik_faecher_fachkollegien.pdf}
 "df.sub"
+#' DFG--Kandidierendenliste 2015
+#' @format A data frame with 9695 rows and 9 variables:
+#' \describe{
+#'      \item{Fach}{Nummer des Fachkollegiums}
+#'      \item{Person}{Name und Vorname der kandidierenden Person}
+#'      \item{Sex}{Geschlecht der kandidierenden Person}
+#'      \item{Nr}{Fachnummer}
+#'      \item{Institution}{Institution der kandidierenden Person}
+#'      \item{Von}{vorschlagende Institution}
+#'      \item{AK}{Anzahl kandidierender Personen}
+#'      \item{AP}{Anzahl zu waehlender Personen}
+#'      \item{Kategorie}{Kategorisierung der vorschlagenden Institutionen in: Hochschule, Fachgesellschaft, Sonstiges}
+#' }
+#' @source \url{http://www.dfg.de/download/pdf/dfg_im_profil/gremien/fachkollegien/fk-wahl2015/fkwahl_2015_kandidierendenliste.pdf}
+"df.dfg"
 
