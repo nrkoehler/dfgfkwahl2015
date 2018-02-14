02764bc6f9e3d18ff53b2a843ef27c1c13422a3b
nrkoehler <norbert.r.koehler@gmail.com>
2018-02-14 21:29:19 +0100

    RENAME variables

--- R/data.R
+++ R/data.R
@@ -17,20 +17,23 @@
 #' DFG--Kandidierendenliste 2015 mit Platzierung und Stimmen
 #' @format A data frame with 9695 rows and 9 variables:
 #' \describe{
-##'     \item{Group}{Wissenschaftsbereich}
-#'      \item{Fach}{Nummer des Fachkollegiums}
-#'      \item{Label}{Name des Fachkollegiums}
-#'      \item{Person}{Name und Vorname der kandidierenden Person}
-#'      \item{Sex}{Geschlecht der kandidierenden Person}
-#'      \item{Nr}{Fachnummer}
-#'      \item{Institution}{Institution der kandidierenden Person}
-#'      \item{Von}{vorschlagende Institution}
-#'      \item{AK}{Anzahl kandidierender Personen}
-#'      \item{AP}{Anzahl zu waehlender Personen}
-#'      \item{Kategorie}{Kategorisierung der vorschlagenden Institutionen in: Hochschule, Fachgesellschaft, Sonstiges}
-#'      \item{Platz}{Platzierung}
-#'      \item{Stimmen}{Anzahl der Stimmen (nur fuer gewaehlte Kandidierende)}
-#'      \item{Vote}{Gewaehlt: TRUE vs. FALSE}
+#'     \item{bereich.nummer}{Wissenschaftsbereich Nummer}
+#'     \item{bereich.name}{Wissenschaftsbereich Name}
+#'     \item{fg.nummer}{Fachgebiet Nummer}
+#'     \item{fg.name}{Fachgebiet Name}
+#'      \item{fk.nummer}{Fachkollegium Nummer}
+#'      \item{fk.name}{Fachkollegium Name}
+#'      \item{fach.nummer}{Fach Nummer}
+#'      \item{person.name}{Name und Vorname der kandidierenden Person}
+#'      \item{person.sex}{Geschlecht der kandidierenden Person}
+#'      \item{person.inst}{Institution der kandidierenden Person}
+#'      \item{inst.von}{vorschlagende Institution}
+#'      \item{inst.von.kat}{Kategorisierung der vorschlagenden Institutionen in: Hochschule, Fachgesellschaft, Sonstiges}
+#'      \item{anzahl.plaetze}{Anzahl zu waehlender Personen}
+#'      \item{anzahl.kandidaten}{Anzahl kandidierender Personen}
+#'      \item{person.platz}{Platzierung der kandidierenden Person}
+#'      \item{person.stimmen}{Anzahl der Stimmen (nur fuer gewaehlte Kandidierende)}
+#'      \item{person.gewaehlt}{Wurde die kandidierende Person gewaehlt? TRUE vs. FALSE}
 #' }
 #' @source \url{http://www.dfg.de/download/pdf/dfg_im_profil/gremien/fachkollegien/fk-wahl2015/fkwahl_2015_kandidierendenliste.pdf}
 #' @source \url{http://www.dfg.de/download/pdf/dfg_im_profil/gremien/fachkollegien/fk-wahl2015/151126_fkwahl_2015_wahlergebnis_vorlaeufig.pdf}
